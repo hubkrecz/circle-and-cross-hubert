8e64942e761caa760eca7753a468a5b3bca44ceb
hubkrecz <107886221+hubkrecz@users.noreply.github.com>
2022-06-30 15:37:10 +0200

    repairing game logic

--- src/script.js
+++ src/script.js
@@ -3,15 +3,25 @@ const circle = document.querySelector("#circle");
 const board = document.querySelector("#board");
 const reset = document.querySelector("#resetButton");
 
-const a1 = board.children[0]
-const a2 = board.children[1]
-const a3 = board.children[2]
-const a4 = board.children[3]
-const a5 = board.children[4]
-const a6 = board.children[5]
-const a7 = board.children[6]
-const a8 = board.children[7]
-const a9 = board.children[8]
+const fields = 
+{
+    "0.0": board.children[0], 
+    "0.1": board.children[1], 
+    "0.2": board.children[2],
+    "1.0": board.children[3],
+    "1.1": board.children[4],
+    "1.2": board.children[5],
+    "2.0": board.children[6],
+    "2.1": board.children[7],
+    "2.2": board.children[8]
+}
+
+const tableMaker = () =>
+{
+    return Array.from({length: 3}, (_, row) => Array.from({length: 3} , (_, cell) => fields[`${row}.${cell}`]))
+}
+
+const justATable = tableMaker();
 
 
 let wwc;
@@ -20,7 +30,6 @@ const start = (event) =>
 {
     const currentButton = event.target;
     const type = currentButton.textContent;
-    const makedTable = tableMaker();
     
     if(type === "O")
     {
@@ -31,58 +40,73 @@ const start = (event) =>
         wwc = 2;
     }
     const buttons = Array.from(board.children);
-    for(let i=0; i < 9; i++)
+    for(let i=0; i <= 8; i++)
     {
         buttons[i].disabled=false;
     }
+
     cross.style.display="none";
     circle.style.display="none";
 }
 
 const resetFunction = function () 
 {
-    const buttons = Array.from(table.children);
-    // const tableCleaner = tableMaker();
+    const buttons = Array.from(board.children);
 
     for (let i = 0; i < 9; i++) 
     {
         buttons[i].innerHTML = "";
-        console.log(buttons[i]);
         buttons[i].disabled = true;
     }
+    for(let i = 0; i <= 2; i++)
+    {
+        for(let j = 0; j <= 2; j++)
+        {
+            justATable[i][j] = null;
+        }  
+    }          
     cross.style.display = "inline-block";
     circle.style.display = "inline-block";
 };
 
 const replaceByXO = (event) =>
 {    
-    const buttonsClicked = event.target;
-    console.log(buttonsClicked);
-    if(buttonsClicked.innerHTML==="")
+    const buttonClicked = event.target;
+    console.log(buttonClicked);
+    if(buttonClicked.innerHTML==="")
     {
-    if(wwc===1){buttonsClicked.innerHTML ="O";wwc=2}
-    else if (wwc===2){buttonsClicked.innerHTML ="X";wwc=1}
-    };
-    const XOtable = tableMaker();
-    for(let i = 0; i <= 2; i++)
-    {
-        for(let j = 0; j <= 2; j++)
+        if(wwc===1)
         {
-            
-            console.log(`${i}.${j}`)
+            buttonClicked.innerHTML ="O";
+            justATable[buttonClicked] = 1;
+            console.log(justATable[1][2]) ;
+            wwc=2;
+        }
+        else if (wwc===2)
+        {
+            buttonClicked.innerHTML ="X";
+            justATable[buttonClicked] = 2;
+            console.log(justATable[buttonClicked]) ;
+            wwc=1
+        }
+        };
+
+        for(let i = 0; i <= 2; i++)
+        {
+            for(let j = 0; j <= 2; j++)
+            {
+                
+            }
         }
     }
-}
+
 
 // const tableReader = () =>
 // {
 //     const [number1, number2] = target.dataset.id.split("."); 
 // }
 
-const tableMaker = () =>
-{
-    return Array.from({length: 3}) , () => Array.from({length: 3} , () => null)
-}
+
 
 // const whoWon = (event) =>
 // {
@@ -93,12 +117,8 @@ const tableMaker = () =>
 reset.addEventListener("click", resetFunction);
 cross.addEventListener("click", start);
 circle.addEventListener("click", start);
-a1.addEventListener("click", replaceByXO);
-a2.addEventListener("click", replaceByXO);
-a3.addEventListener("click", replaceByXO);
-a4.addEventListener("click", replaceByXO);
-a5.addEventListener("click", replaceByXO);
-a6.addEventListener("click", replaceByXO);
-a7.addEventListener("click", replaceByXO);
-a8.addEventListener("click", replaceByXO);
-a9.addEventListener("click", replaceByXO);
+Object.values(fields).forEach((field)=>
+{
+    field.addEventListener("click", replaceByXO);
+    console.log(field);
+});
